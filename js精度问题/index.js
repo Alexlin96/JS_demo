7129988f7c273e5909072f604c4334f4295eaf4b
linzhiqiang <linzhiqiang@goatgames.com>
2021-03-01 18:04:09 +0800

    03-01

---
+++ js精度问题/index.js
@@ -0,0 +1,7 @@
+function add(num1, num2) {
+  const num1Digits = (num1.toString().split(".")[1] || "").length;
+  const num2Digits = (num2.toString().split(".")[1] || "").length;
+  const baseNum = Math.pow(10, Math.max(num1Digits, num2Digits));
+  return (num1 * baseNum + num2 * baseNum) / baseNum;
+}
+add(0.1, 0.2)
